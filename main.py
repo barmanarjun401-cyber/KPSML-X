b7d7b6af5a631403a5f650eb3ad51e91f1983d75
barmanarjun401-cyber <barmanarjun401@gmail.com>
2025-12-04 14:37:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,3 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Abanime Flask server is running!"
-
-# Vercel ko sirf 'app' object chahiye, isliye yaha pe
-# if __name__ == "__main__": wala part zaroori nahi hai.
